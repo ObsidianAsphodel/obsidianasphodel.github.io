8fb7a2416be83f533e9e3d47ea5b53b7226c642f
ObsidianAsphodel <terencep.dixon@gmail.com>
2025-07-13 17:41:03 -0700

    changes to app

--- React-Project-2025/Somnambulist Project/src/App.jsx
+++ React-Project-2025/Somnambulist Project/src/App.jsx
@@ -1,8 +1,12 @@
-import Message from "./message"
+import Message from "./components/message";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 function App(){
   return <div>
-            <Message/>
+            <Header/>
+            <Message></Message>
+            <Footer></Footer>
         </div>
 }
 
---
+++ React-Project-2025/Somnambulist Project/src/components/header.jsx
@@ -0,0 +1,18 @@
+function Header (){
+    return <header>
+        <h1>
+            <a>Somnambulist Project</a>
+        </h1>
+        <h3>
+            Waking Dreamer
+        </h3>
+        <nav>
+            <li>
+                <ul><a>About</a></ul>
+                <ul><a>Digital Works</a></ul>
+                <ul><a>Analog Works</a></ul>
+            </li>
+        </nav>
+    </header>
+}
+export default Header
